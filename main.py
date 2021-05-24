ef69a6528b2f221c667c7d2e01c859445fa1a702
QBx <ortynskiy-k.ros@mail.ru>
2021-05-24 19:19:48 +0300

    Решена 3 задача

--- main.py
+++ main.py
@@ -21,3 +21,22 @@ odd_to_2 = (num for num in range(1, get_num+1, 2))
 
 for i in range(1, get_num + 1, 2):
     print(next(odd_to_2))
+
+# Задача 3
+
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А'
+]
+
+for i in range(len(tutors) - len(klasses)):
+    if len(tutors) - len(klasses) > 0:
+        klasses.append(None)
+
+tupple_new = ((v_1, v_2) for v_1, v_2 in zip(tutors, klasses))
+
+print(f'Это генератор: {type(tupple_new)}')
+print(*tupple_new)
